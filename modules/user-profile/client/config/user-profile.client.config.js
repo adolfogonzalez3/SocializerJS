f5f359b14ee40e70ad16d0fdb50614f1a28f7b74
Matias Barcenas <MPX.4132@Gmail.com>
2016-12-14 13:01:13 -0600

    Spacing & App Name
    Fixed spacing issues, a dependency issue and renamed the app template.

--- modules/user-profile/client/config/user-profile.client.config.js
+++ modules/user-profile/client/config/user-profile.client.config.js
@@ -1,20 +1,20 @@
 (function() {
   'use strict';
-
+  
   // User profile module config
   angular
-    .module('user-profile')
-    .run(menuConfig);
-
+  .module('user-profile')
+  .run(menuConfig);
+  
   menuConfig.$inject = ['Menus'];
-
+  
   function menuConfig(Menus) {
     // Config logic
     // ...
     Menus.addMenuItem('topbar', { // I manually added this part here (Matt).
-	   title: 'Profile',
-	   state: 'profile',
-	   roles: ['*'] 
+      title: 'Profile',
+      state: 'profile',
+      roles: ['*'] 
     });
   }
 })();
